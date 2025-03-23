8c40b3231adcd9b9e717422cbcab09527b2c81a0
thomas <thomas.lemagoariec@outlook.com>
2025-03-23 10:46:47 +0100

    auto

--- modules/dev/editors/flake.nix
+++ modules/dev/editors/flake.nix
@@ -1,7 +1,6 @@
 {
   description = "Editors module flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs = { nixpkgs, ... }: {
     nixosModules.editors = { config, pkgs, ... }: {
