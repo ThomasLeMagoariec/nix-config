3e0af90feab5de6711bac6c707cb085a2ca7e0f5
thomas <thomas.lemagoariec@outlook.com>
2025-04-09 17:04:10 +0200

    auto

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -19,6 +19,31 @@
     programs = {
         zsh = {
             enable = true;
+            enableCompletion = true;
+            syntaxHighLighting.enable = true;
+            autosuggestions.enable = true;
+
+            shellAliases = {
+                gs = "git status";
+                gac = "git add -A && git commit -m ";
+                gaca = "git add -A && git commit -m 'auto'";
+                gacp = "git add -A && git commit -m 'auto' && git push";
+            };
+
+            initExtra = ''
+                export EDITOR=nvim
+                bindkey '^P' up-history
+                bindkey '^N' down-history
+            '';
+
+            plugins = [
+                { name = "zsh-autosuggestions"; src = pkgs.zsh-autosuggestions; }
+                { name = "zsh-syntax-highlighting"; src = pkgs.zsh-syntax-highlighting; }
+            ];
+
+            promptInit = ''
+                source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+            '';
         };
     };
 
