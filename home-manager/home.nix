bfe4612f169a664308c16dae37d7f3fc4f1594cc
thomas <thomas.lemagoariec@outlook.com>
2025-04-09 17:08:42 +0200

    auto

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,52 +1,56 @@
 { config, pkgs, ... }:
 
 {
-    home.username = "thomas";
-    home.homeDirectory = "/home/thomas";
+  home.username = "thomas";
+  home.homeDirectory = "/home/thomas";
 
-    home.stateVersion = "24.11"; # Please read the comment before changing.
+  home.stateVersion = "24.11";
 
-    home.packages = [
-    ];
+  home.packages = [];
 
-    home.file = {
+  home.file = {};
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  programs = {
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      syntaxHighlighting.enable = true;  # ✅ fixed spelling
+      # autosuggestions.enable = true;   ❌ remove this
+
+      shellAliases = {
+        gs = "git status";
+        gac = "git add -A && git commit -m ";
+        gaca = "git add -A && git commit -m 'auto'";
+        gacp = "git add -A && git commit -m 'auto' && git push";
+      };
+
+      initExtra = ''
+        export EDITOR=nvim
+        bindkey '^P' up-history
+        bindkey '^N' down-history
+      '';
+
+      plugins = [
+        {
+          name = "zsh-autosuggestions";
+          src = pkgs.zsh-autosuggestions;
+        }
+        {
+          name = "zsh-syntax-highlighting";
+          src = pkgs.zsh-syntax-highlighting;
+        }
+      ];
+
+      promptInit = ''
+        source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+      '';
     };
+  };
 
-    home.sessionVariables = {
-        EDITOR = "nvim";
-    };
-
-    programs = {
-        zsh = {
-            enable = true;
-            enableCompletion = true;
-            syntaxHighLighting.enable = true;
-            autosuggestions.enable = true;
-
-            shellAliases = {
-                gs = "git status";
-                gac = "git add -A && git commit -m ";
-                gaca = "git add -A && git commit -m 'auto'";
-                gacp = "git add -A && git commit -m 'auto' && git push";
-            };
-
-            initExtra = ''
-                export EDITOR=nvim
-                bindkey '^P' up-history
-                bindkey '^N' down-history
-            '';
-
-            plugins = [
-                { name = "zsh-autosuggestions"; src = pkgs.zsh-autosuggestions; }
-                { name = "zsh-syntax-highlighting"; src = pkgs.zsh-syntax-highlighting; }
-            ];
-
-            promptInit = ''
-                source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-            '';
-        };
-    };
-
-
-    programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 }
+
