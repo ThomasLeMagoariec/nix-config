e20bc9e056e43faf652ee6e693f8e5faf3b56d27
thomas <thomas.lemagoariec@outlook.com>
2025-04-09 17:18:17 +0200

    auto

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,53 +1,57 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "thomas";
-  home.homeDirectory = "/home/thomas";
+    home.username = "thomas";
+    home.homeDirectory = "/home/thomas";
 
-  home.stateVersion = "24.11";
+    home.stateVersion = "24.11";
 
-  home.packages = [];
+    home.packages = [];
 
-  home.file = {};
-
-  home.sessionVariables = {
-    EDITOR = "nvim";
-  };
-
-  programs = {
-    zsh = {
-      enable = true;
-      enableCompletion = true;
-      syntaxHighlighting.enable = true;
-
-      shellAliases = {
-        gs = "git status";
-        gac = "git add -A && git commit -m ";
-        gaca = "git add -A && git commit -m 'auto'";
-        gacp = "git add -A && git commit -m 'auto' && git push";
-      };
-
-      initExtra = ''
-        source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-        export EDITOR=nvim
-        bindkey '^P' up-history
-        bindkey '^N' down-history
-      '';
-
-      plugins = [
-        {
-          name = "zsh-autosuggestions";
-          src = pkgs.zsh-autosuggestions;
-        }
-        {
-          name = "zsh-syntax-highlighting";
-          src = pkgs.zsh-syntax-highlighting;
-        }
-      ];
-
+    home.file = {
+        ".p10k.zsh".source = ./dotfiles/p10k.zsh;
     };
-  };
 
-  programs.home-manager.enable = true;
+    home.sessionVariables = {
+        EDITOR = "nvim";
+    };
+
+    programs = {
+        zsh = {
+            enable = true;
+            enableCompletion = true;
+            syntaxHighlighting.enable = true;
+
+            shellAliases = {
+                gs = "git status";
+                gac = "git add -A && git commit -m ";
+                gaca = "git add -A && git commit -m 'auto'";
+                gacp = "git add -A && git commit -m 'auto' && git push";
+            };
+
+            initExtra = ''
+                source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+                [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
+
+                export EDITOR=nvim
+                bindkey '^P' up-history
+                bindkey '^N' down-history
+            '';
+
+            plugins = [
+                {
+                    name = "zsh-autosuggestions";
+                    src = pkgs.zsh-autosuggestions;
+                }
+                {
+                    name = "zsh-syntax-highlighting";
+                    src = pkgs.zsh-syntax-highlighting;
+                }
+            ];
+
+        };
+    };
+
+    programs.home-manager.enable = true;
 }
 
