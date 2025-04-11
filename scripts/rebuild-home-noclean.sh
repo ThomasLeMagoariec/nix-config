d0fcfa9018ba77e4c3baebd58b073f18d844f332
thomas <thomas.lemagoariec@outlook.com>
2025-04-11 08:59:20 +0200

    more scripts

---
+++ scripts/rebuild-home-noclean.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Check for host argument
+if [ -z "$1" ]; then
+  echo "❌ Usage: $0 <hostname>"
+  exit 1
+fi
+
+HOST=$1
+USERNAME=$(whoami)
+
+# update flake
+./update.sh
+
+# rebuild home-manager
+./build-home.sh ${USERNAME}
+
+echo "✅ Done rebuilding for host '$HOST' and user '$USERNAME'"
+
