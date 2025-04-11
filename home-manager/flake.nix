663f324e9664e5d3258eb475df99190e30cd780c
thomas <thomas.lemagoariec@outlook.com>
2025-04-11 09:45:03 +0200

    cleanup

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -17,15 +17,11 @@
     in {
 
         # it is possible to configure different users
-        homeConfigurations."thomas" = home-manager.lib.homeManagerConfiguration {
-            inherit pkgs;
-
-            # Specify your home configuration modules here, for example,
-            # the path to your home.nix.
-            modules = [ ../hosts/thomas/home.nix ];
-
-            # Optionally use extraSpecialArgs
-            # to pass through arguments to home.nix
+        homeConfigurations = {
+            "thomas" = home-manager.lib.homeManagerConfiguration {
+                inherit pkgs;
+                modules = [ ../hosts/thomas/home.nix ];
+            };
         };
     };
 }
