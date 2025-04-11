d5abf5802a4b4df3b4c905a71eeca09308b395d5
thomas <thomas.lemagoariec@outlook.com>
2025-04-11 16:45:35 +0200

    added paul user

--- modules/system/users.nix
+++ modules/system/users.nix
@@ -1,13 +1,29 @@
-{ config, pkgs, ... }:
+{ config, pkgs, host, ... }:
 
 {
     users.defaultUserShell=pkgs.zsh;
-    users.users.thomas = {
-        isNormalUser = true;
-        description = "Thomas";
-        extraGroups = [ "networkmanager" "wheel" "docker" "video" ];
-        packages = with pkgs; [
-            #  use home-manager for this
-        ];
-    };
+
+    users.users = lib.mkMerge [
+        (lib.mkIf (host == "thomas") {
+            thomas = {
+                isNormalUser = true;
+                description = "Thomas";
+                extraGroups = [ "networkmanager" "wheel" "docker" "video" ];
+                packages = with pkgs; [
+                    # use home-manager for this
+                ];
+            };
+        })
+
+        (lib.mkif (host == "paul") {
+            paul = {
+                isNormalUser = true;
+                description = "Paul";
+                extraGroups = [ "networkmanager" "wheel" "docker" "video" ];
+                packages = with pkgs; [
+                    # use home-manager for this
+                ];
+            };
+        })
+    ];
 }
