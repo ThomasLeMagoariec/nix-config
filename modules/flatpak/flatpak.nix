263e620fdd6b9d7620af1addc1b71adb13b3080d
thomas <thomas.lemagoariec@outlook.com>
2025-04-11 21:06:02 +0200

    fix: flatpak

--- modules/flatpak/flatpak.nix
+++ modules/flatpak/flatpak.nix
@@ -9,9 +9,19 @@
 
     systemd.user.services.flatpak-updater = {
         description = "Flatpak app updater";
-        serviceConfig.ExecStart = "${pkgs.flatpak}/bin/flatpak update -y";
-        timerConfig.OnCalendar = "daily";
+        serviceConfig = {
+            Type = "oneshot";
+            ExecStart = "${pkgs.flatpak}/bin/flatpak update -y";
+        };
+    };
+
+    systemd.user.timers.flatpak-updater = {
+        description = "Run daily Flatpak updates";
         wantedBy = [ "timers.target" ];
+        timerConfig = {
+            OnCalendar = "daily";
+            Persistent = true;
+        };
     };
 }
 
