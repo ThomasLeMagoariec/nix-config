279a57378d7eb361f1c080d334ad5e39a9c7dc54
thomas <thomas.lemagoariec@outlook.com>
2025-04-13 21:26:16 +0200

    added flex

--- modules/lfn/lfn.nix
+++ modules/lfn/lfn.nix
@@ -2,23 +2,6 @@
 
 {
     environment.systemPackages = with pkgs; [
-        bc
-        binutils
-        bison
         flex
-        gcc
-        git
-        gnupg24
-        gzip
-        libelf
-        libncurses5-dev
-        libssl-dev
-        make
-        openssl
-        pahole
-        perl-base
-        rsync
-        tar
-        xz-utils
     ];
 }
