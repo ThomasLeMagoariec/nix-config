29637043c92bc52bbb0ac6f7f2e80ca798cbff07
thomas <thomas.lemagoariec@outlook.com>
2025-04-14 21:53:22 +0200

    moved stuff from lfn to shell

--- shells/elfutils.nix
+++ shells/elfutils.nix
@@ -2,11 +2,32 @@
 { pkgs ? import <nixpkgs> {} }:
 
 pkgs.mkShell {
-  buildInputs = with pkgs; [
-    elfutils
-  ];
-  nativeBuildInputs = with pkgs; [
-    pkg-config
-  ];
+    buildInputs = with pkgs; [
+        elfutils
+        flex
+        bison
+        ncurses
+        pkg-config
+        elfutils
+        libelf
+        bc
+        autoconf
+        automake
+        gettext
+        gperf
+        m4
+        libtool
+    ];
+
+    nativeBuildInputs = with pkgs; [
+        pkg-config
+    ];
+
+    shellHook = ''
+        export FORCE_UNSAFE_CONFIGURE=1
+        export CONFIG_SHELL=${pkgs.bash}/bin/bash
+        export LFN=/home/thomas/coding/lfs/root
+
+    '';
 }
 
