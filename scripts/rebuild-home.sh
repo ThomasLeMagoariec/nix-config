eabac9154e26decca87206c80aba399efea5e37d
thomas <thomas.lemagoariec@outlook.com>
2025-04-20 19:27:37 +0200

    fix: rebuild home script

--- scripts/rebuild-home.sh
+++ scripts/rebuild-home.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# Check for host argument
+if [ -z "$1" ]; then
+  echo "❌ Usage: $0 <hostname>"
+  exit 1
+fi
+
 echo "🏡 Rebuilding Home Manager for user '$1'..."
 home-manager switch --flake ../home-manager#${1}
 
