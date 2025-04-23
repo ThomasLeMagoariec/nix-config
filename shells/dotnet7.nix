8bf33da254ba037382265883c30ed1696e9bceb0
thomas <thomas.lemagoariec@outlook.com>
2025-04-23 12:02:50 +0200

    change:  dotnet7

--- shells/dotnet7.nix
+++ shells/dotnet7.nix
@@ -1,14 +1,13 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-23.05.tar.gz") {} }:
 
 pkgs.mkShell {
-  buildInputs = [
-    pkgs.dotnet-sdk_7
-  ];
+    buildInputs = [
+        pkgs.dotnet-sdk_7
+    ];
 
-  shellHook = ''
-    echo "Using an older Nixpkgs version with .NET 7!"
-    dotnet --version
-    zsh
-  '';
+    shellHook = ''
+        echo "Using an older Nixpkgs version with .NET 7!"
+        dotnet --version
+    '';
 }
 
