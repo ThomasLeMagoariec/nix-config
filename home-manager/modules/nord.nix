a49425926245a90730b6341e8d59787af2472e03
thomas <thomas.lemagoariec@outlook.com>
2025-04-30 22:30:11 +0200

    theme selector

--- home-manager/modules/nord.nix
+++ home-manager/modules/nord.nix
@@ -5,6 +5,7 @@
         ".config/hypr/theme.conf".source = ../dotfiles/hypr/nord.conf;
         ".config/hypr/wallpaper.png".source = ../dotfiles/hypr/wallpaper-nord.png;
         ".config/hypr/lockscreen.png".source = ../dotfiles/hypr/lockscreen-nord.png;
+        ".config/hypr/hyprlock.conf".source = ../dotfiles/hypr/hyprlock-nord.conf;
     };
 
 }
