bdaa4c814ba69e5ed019c3ad9904d3d35edc0b2d
thomas <thomas.lemagoariec@outlook.com>
2025-05-02 15:48:18 +0200

    catpuccin: dotfiles

--- home-manager/modules/catpuccin.nix
+++ home-manager/modules/catpuccin.nix
@@ -3,8 +3,8 @@
 {
     home.file = {
         ".config/hypr/theme.conf".source = ../dotfiles/hypr/catpuccin.conf;
-        ".config/hypr/wallpaper.png".source = ../dotfiles/hypr/wallpaper-catpuccin.jpg;
-        ".config/hypr/lockscreen.png".source = ../dotfiles/hypr/lockscreen-catpuccin.jpg;
+        ".config/hypr/wallpaper.png".source = ../dotfiles/hypr/wallpaper-catpuccin.png;
+        ".config/hypr/lockscreen.png".source = ../dotfiles/hypr/lockscreen-catpuccin.png;
         ".config/hypr/hyprlock.conf".source = ../dotfiles/hypr/hyprlock-catpuccin.conf;
         ".config/waybar/style.css".source = ../dotfiles/waybar/style-catpuccin.css;
     };
