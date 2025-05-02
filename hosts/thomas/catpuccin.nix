abf9ae61f980f0faf09d05b2b893f6259af32cfc
thomas <thomas.lemagoariec@outlook.com>
2025-05-02 15:47:12 +0200

    catpuccin: wallpaper + lockscreen

--- hosts/thomas/catpuccin.nix
+++ hosts/thomas/catpuccin.nix
@@ -12,7 +12,7 @@ in
         ../../home-manager/modules
     ]
     ++ lib.optional (theme == "rose-pine") ../../home-manager/modules/rose-pine.nix
-    ++ lib.optional (theme == "nord") ../../home-manager/modules/nord.nix;
+    ++ lib.optional (theme == "nord") ../../home-manager/modules/nord.nix
     ++ lib.optional (theme == "catpuccin") ../../home-manager/modules/catpuccin.nix;
 
     home.username = "thomas";
