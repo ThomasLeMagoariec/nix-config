2dbed69ad9ec00ccf56cc10e301909f44ed7b30e
thomas <thomas.lemagoariec@outlook.com>
2025-06-23 20:58:23 +0200

    choose-theme.sh

--- scripts/wofi/choose-theme.sh
+++ scripts/wofi/choose-theme.sh
@@ -5,11 +5,11 @@ CHOICE=$(echo -e "rosé-pine\ncatppuccin\nnord" | wofi --dmenu --prompt "Choose 
 
 case "$CHOICE" in
     rosé-pine)
-        cd .. && ./rebuild-home-rosepine.sh;;
+        cd .. && ./rebuild-home-rosepine.sh && notify-send "Rosé Pine!";;
     catppuccin)
-        cd .. && ./rebuild-home-catppuccin.sh;;
+        cd .. && ./rebuild-home-catppuccin.sh && notify-send "Catppuccin!";;
     nord)
-        cd .. && ./rebuild-home-nord.sh;;
+        cd .. && ./rebuild-home-nord.sh && notify-send "Nord!";;
     *)
         exit 1 ;;  # in case of no selection or exit
 esac
