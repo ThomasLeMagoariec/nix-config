c3774e684faf45dc4c028f184f12cac696ffde93
thomas <thomas.lemagoariec@outlook.com>
2025-06-23 19:46:30 +0200

    add: script to change theme via wofi

---
+++ scripts/wofi/choose-theme.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Show custom options in wofi
+CHOICE=$(echo -e "rosé-pine\ncatppuccin\nnord" | wofi --dmenu --prompt "Choose action:")
+
+case "$CHOICE" in
+    rosé-pine)
+        cd .. && ./rebuild-home-rosepine.sh;;
+    catppuccin)
+        cd .. && ./rebuild-home-catppuccin.sh;;
+    nord)
+        cd .. && ./rebuild-home-nord.sh;;
+    *)
+        exit 1 ;;  # in case of no selection or exit
+esac
+
