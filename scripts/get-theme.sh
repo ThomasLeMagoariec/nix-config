2538b73a118bf0c722a5ed5966122638e1b0c741
thomas <thomas.lemagoariec@outlook.com>
2025-06-25 20:27:10 +0200

    get-theme

--- scripts/get-theme.sh
+++ scripts/get-theme.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
 
-# Path to your theme file
-theme_file="./theme.txt"
-
-if [ -f "$theme_file" ]; then
-    theme=$(cat "$theme_file")
-    echo "$theme"
-else
-    # default value
-    echo "rose-pine"
-fi
+echo $THEME
