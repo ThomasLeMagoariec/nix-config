46fdc10b0e58b581db26228427efa5d139c96360
thomas <thomas.lemagoariec@outlook.com>
2025-07-07 19:34:53 +0200

    grub

--- modules/dev/grub.nix
+++ modules/dev/grub.nix
@@ -8,8 +8,8 @@ let
     src = pkgs.fetchFromGitHub {
       owner = "catppuccin";
       repo = "grub";
-      rev = "7ceef1457ff5bafe0ed61f46df87d352dc6e641f"; # or use latest commit
-      sha256 = "sha256-LPjEcZrXlC+BLAGs4B0LV/HjeZrRX3Y8HQ5FV3l6Yl8=";
+      rev = "2a5c8be8185dae49dd22030df45860df8c796312"; # or use latest commit
+      sha256 = "sha256-20D1EcV8SWOd5BLdAc6FaQu3onha0+aS5yA/GK8Ra0g=";
     };
 
     installPhase = ''
